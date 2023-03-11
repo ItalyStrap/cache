8b6b2724dd7a22ee800e981f8e7cf1ec2005e008
Enea <info@overclokk.net>
2023-03-11 16:48:50 +0100

    Deleted test.bat

--- test.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-vendor\bin\phpcbf --ignore=./tests/_support/* ./src/ ./tests/ && vendor\bin\phpcs --ignore=./tests/_support/* ./src/ ./tests/ && vendor\bin\phpstan analyze --level=max && codecept run unit && codecept run wpunit
