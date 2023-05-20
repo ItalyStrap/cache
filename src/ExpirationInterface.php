129c42a89af9b9d1041aae271a11cf93a2beb3f6
Enea <info@overclokk.net>
2023-05-20 18:00:32 +0200

    CS:FIX

--- src/ExpirationInterface.php
+++ src/ExpirationInterface.php
@@ -7,11 +7,11 @@ interface ExpirationInterface
 {
 
     public const YEAR_IN_SECONDS = 31_622_400;
-	public const MONTH_IN_SECONDS = 2_592_000;
-	public const WEEK_IN_SECONDS = 604_800;
-	public const DAY_IN_SECONDS = 86_400;
-	public const HOUR_IN_SECONDS = 3_600;
-	public const MINUTE_IN_SECONDS = 60;
+    public const MONTH_IN_SECONDS = 2_592_000;
+    public const WEEK_IN_SECONDS = 604_800;
+    public const DAY_IN_SECONDS = 86_400;
+    public const HOUR_IN_SECONDS = 3_600;
+    public const MINUTE_IN_SECONDS = 60;
 
     public function isValid(): bool;
 
